421b9ea1e361f82d2e49a17078ba34813f8480db
Axel López <60415328+Latzel@users.noreply.github.com>
2021-12-06 12:43:50 -0800

    Update puntero.s

--- puntero.s
+++ puntero.s
@@ -7,13 +7,15 @@
 .data
 var1: .word 3
 puntero_var1: var1
+/*Se define el puntero como var1*/
 
 
 .text
 .global main
 
-main: ldr r0, =puntero_var1 
+main: ldr r0, =puntero_var1 /*Son necesarios dos ldr para accerder al valor del puntero*/
   ldr r1, [r0]
   ldr r2, [r1]
+  /*En este punto los valores de r1 y r3 son iguales*/
   ldr r3, =var1
   bx  lr
