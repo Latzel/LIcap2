3dc447a3de22c11678106c35b340d40b6707df74
Axel López <60415328+Latzel@users.noreply.github.com>
2021-12-06 13:21:25 -0800

    Create sumavectores.s

---
+++ sumavectores.s
@@ -0,0 +1,25 @@
+@@ Instituto Tecnologico de Tijuana
+@@ Depto de Sistemas y Computacion
+@@ Ing. En Sistemas Computacionales
+@@ Autor: Axel Josue De La Rosa Lopez @nickname: Latzel
+@@ Fecha de revision: 26/11/21
+
+.data
+var1: .asciz  "La suma es %d\n"
+var2: .word 128, 32, 100, -30, 124
+
+.text
+.global main
+
+main: push  {r4, lr}
+  mov r0, #5
+  mov r1, #0
+  ldr r2, =var2
+  bucle:  ldr r3, [r2], #4
+    add r1, r1, r3
+    subs  r0, r0, #1
+    bne bucle
+    ldr r0, =var1
+    bl  printf
+    pop {r4, lr}
+    bx  lr
