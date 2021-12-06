9db3bb508f86249984e626191fbaac43329267ec
Axel López <60415328+Latzel@users.noreply.github.com>
2021-12-06 13:23:36 -0800

    Update sumavectores.s

--- sumavectores.s
+++ sumavectores.s
@@ -12,14 +12,18 @@ var2: .word 128, 32, 100, -30, 124
 .global main
 
 main: push  {r4, lr}
+//Se inicializan las variables y apuntamos r2 a var2
   mov r0, #5
   mov r1, #0
   ldr r2, =var2
+  //Bucle para realizar la suma
   bucle:  ldr r3, [r2], #4
     add r1, r1, r3
     subs  r0, r0, #1
-    bne bucle
+    bne bucle //Nuevo llamado al bucle
+    //Aqui se imprime el resultado
     ldr r0, =var1
     bl  printf
+    //Recuperamos registros y salimos
     pop {r4, lr}
     bx  lr
