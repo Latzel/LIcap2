99dacbdb2ee87ba5ccdaa75dc0598fed1e90bd02
Axel López <60415328+Latzel@users.noreply.github.com>
2021-12-06 13:08:07 -0800

    Update ejemplofor.s

--- ejemplofor.s
+++ ejemplofor.s
@@ -13,12 +13,20 @@ main:
   push  {r4, lr}
   mov r4, #0 //Se asigna el valor de 0 a r4
   
-bfor:
+bfor: //aqui inicia el ciclo
   mov r1, r4
   ldr r0, =var1
-  add r4, r4, #1
+  add r4, r4, #1 //el valor de r4 se convierte a r4 + 1
   bl  printf
-  cmp r4, #6
+  cmp r4, #6 //Comparacion para el ciclo
   bne bfor
   pop {r4, pc}
+  pop {r4, pc} 
+  /* 
+  Se utilizo la simplificacion de:
+  pop {r4, lr}
+  bx  lr
+  
+  por:
   pop {r4, pc}
+  */
