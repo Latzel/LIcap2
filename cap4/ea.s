5a6dab5ad3ac9242f1d82d686f414996b4706e8c
Axel López <60415328+Latzel@users.noreply.github.com>
2021-12-08 01:19:44 -0800

    Update ea.s

--- cap4/ea.s
+++ cap4/ea.s
@@ -10,13 +10,13 @@
   .set GPCLR0, 0x28
   
 .text
-ldr r0, =GPBASE
+ ldr r0, =GPBASE
 //guia bits xx999888777666555444333222111000
-mov r1, #0b00001000000000000000000000000000
-str r1, [r0, #GPFSEL0] //Configura como salida
+ mov r1, #0b00001000000000000000000000000000
+ str r1, [r0, #GPFSEL0] //Configura como salida
 //guia bits 10987654321098765432109876543210 
 bucle: mov  r1, #0b00000000000000000000001000000000
-str r1, [r0, #GPSET0] //Enciende
-mov r1, #0b00000000000000000000001000000000
-str r1, [r0,  #GPCLR0] //Apaga
-b bucle
+ str r1, [r0, #GPSET0] //Enciende
+ mov r1, #0b00000000000000000000001000000000
+ str r1, [r0,  #GPCLR0] //Apaga
+ b bucle
