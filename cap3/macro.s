c18e40c8843d5a8a4d19615373d3352a67fbc11c
Axel López <60415328+Latzel@users.noreply.github.com>
2021-12-07 20:48:12 -0800

    Update macro.s

--- cap3/macro.s
+++ cap3/macro.s
@@ -6,28 +6,29 @@
 
 
 .macro abs
-tst r0, r0
-negmi r0, r0
+  tst r0, r0
+  negmi r0, r0
+  
 .endm
 .data
-var1 : .asciz " %d\ n"
+var1: .asciz " %d\ n"
 .text
 .global main
-/* Salvo registros */
-main : push { r4, lr }
-/* Primera llamada abs (1) */
-mov r0, # 1
-bl abs
-/* Imprimo primera llamada */
+//Salvo registros
+main: push {r4, lr}
+//Primera llamada abs 1
+mov r0, #1
+bl  abs
+//Imprimo primera llamada
 mov r1, r0
-ldr r0, = var1
-bl printf
-/* Segunda llamada abs (-2) e imprimo */
-mov r0, # -2
-bl abs
+ldr r0, =var1
+bl  printf
+//Segunda llamada abs (-2)
+mov r0, #-2
+bl  abs
 mov r1, r0
-ldr r0, = var1
-bl printf
+ldr r0, =var1
+bl  printf
 /* Tercera llamada abs (3) e imprimo */
 mov r0, # 3
 bl abs
