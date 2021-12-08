85810b5deec3b60b52d8ab51cb2e66d741a7d510
Axel López <60415328+Latzel@users.noreply.github.com>
2021-12-08 01:05:46 -0800

    Update hm.s

--- cap4/hm.s
+++ cap4/hm.s
@@ -10,12 +10,12 @@ cadenafin:
 .text
 .global main
 
-main: push { r7, lr } //preservamos reg. 
+main: push { r7, lr }            //preservamos reg. 
 mov r0, #1 
-ldr r1, =cadena //cadena a enviar 
-mov r2, #cadenafin - cadena //longitud 
+ldr r1, =cadena                 //cadena a enviar 
+mov r2, #cadenafin - cadena     //longitud 
 mov r7, #4
-swi #0 //llamada a sistema "write"
+swi #0                          //llamada a sistema "write"
 mov r0, #0 //devolvemos ok
-pop {r7, lr} //recuperamos reg.
-bx  lr //salida
+pop {r7, lr}                    //recuperamos reg.
+bx  lr                          //salida
