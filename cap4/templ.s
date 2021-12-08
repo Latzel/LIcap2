4ca457dce6cbdfb839c8e8ce0bbfd01e6cea601d
Axel López <60415328+Latzel@users.noreply.github.com>
2021-12-08 01:18:40 -0800

    Update templ.s

--- cap4/templ.s
+++ cap4/templ.s
@@ -6,23 +6,26 @@
   .set STCLO, 0x04
   
 .text
-ldr r0, =GPBASE
+  ldr r0, =GPBASE
 //guia bits xx999888777666555444333222111000
-mov r1, #0b00001000000000000000000000000000
-str r1, [r0, #GPFSEL0] //Configura GPIO 9
+  mov r1, #0b00001000000000000000000000000000
+  str r1, [r0, #GPFSEL0] //Configura GPIO 9
 //guia bits 10987654321098765432109876543210
-mov r1, #0b00000000000000000000001000000000
-ldr r2, =STBASE
+  mov r1, #0b00000000000000000000001000000000
+  ldr r2, =STBASE
+  
 bucle: bl espera //Salta a rutina de espera
-str r1, [r0, #GPSET0]
-bl  espera //salta a rutina de espera
-str r1, [r0, #GPCLR0 ]
-b bucle
+  str r1, [r0, #GPSET0]
+  bl  espera //salta a rutina de espera
+  str r1, [r0, #GPCLR0 ]
+  b bucle
+  
 //rutina que espera medio segundo
 espera: ldr r3, [r2, #STCLO ] //Lee contador en r3
-ldr r4, =500000
-add r4, r3 //r4= r3+ medio millón
+  ldr r4, =500000
+  add r4, r3 //r4= r3+ medio millón
+  
 ret1: ldr r3, [r2, #STCLO]
-cmp r3,r4 //leemos CLO hasta alcanzar
-bne ret1 //el valor de r4
-bx  lr
+  cmp r3,r4 //leemos CLO hasta alcanzar
+  bne ret1 //el valor de r4
+  bx  lr
