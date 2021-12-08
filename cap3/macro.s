31ee0fa67780b0989471946b0faaf156eaf2bca0
Axel López <60415328+Latzel@users.noreply.github.com>
2021-12-07 20:52:44 -0800

    Update macro.s

--- cap3/macro.s
+++ cap3/macro.s
@@ -17,36 +17,36 @@ var1: .asciz "%d\ n"
 //Salvo registros
 main: push {r4, lr}
 //Primera llamada abs 1
-mov r0, #1
-bl  abs
+  mov r0, #1
+  bl  abs
 //Imprimo primera llamada
-mov r1, r0
-ldr r0, =var1
-bl  printf
+  mov r1, r0
+  ldr r0, =var1
+  bl  printf
 
 //Segunda llamada abs (-2)
-mov r0, #-2
-bl  abs
-mov r1, r0
-ldr r0, =var1
-bl  printf
+  mov r0, #-2
+  bl  abs
+  mov r1, r0
+  ldr r0, =var1
+  bl  printf
 
 //Tercera llamada abs (3)
-mov r0, #3
-bl  abs
-mov r1, r0
-ldr r0, = var1
-bl  printf
+  mov r0, #3
+  bl  abs
+  mov r1, r0
+  ldr r0, = var1
+  bl  printf
 
 //Cuarta llamada abs (-4)
-mov r0, #-4
-bl  abs
-mov r1, r0
-ldr r0, =var1
-bl  printf
-pop {r4, lr}
-bx  lr
+  mov r0, #-4
+  bl  abs
+  mov r1, r0
+  ldr r0, =var1
+  bl  printf
+  pop {r4, lr}
+  bx  lr
 
 abs:  tst r0, r0 //comprueba el flag de signo
-negmi r0, r0 //si es negativo, negamos de nuevo
-bx  lr
+  negmi r0, r0 //si es negativo, negamos de nuevo
+  bx  lr
