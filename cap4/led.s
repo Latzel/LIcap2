de15c2400a2194fce5d2096250a953b7198d10a3
Axel López <60415328+Latzel@users.noreply.github.com>
2021-12-08 01:10:10 -0800

    Update led.s

--- cap4/led.s
+++ cap4/led.s
@@ -11,4 +11,4 @@ str r1, [r0, #GPFSEL0] //Configura GPIO 9
 //guia bits 10987654321098765432109876543210
 mov r1, #0b00000000000000000000001000000000
 str r1, [r0, #GPSET0] //Enciende GPIO 9
-infi: binfi
+infi: b infi
