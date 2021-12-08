307a49f63f84fb4dd8506ee71f0e9d2f42331105
Axel López <60415328+Latzel@users.noreply.github.com>
2021-12-07 20:52:06 -0800

    Update macro.s

--- cap3/macro.s
+++ cap3/macro.s
@@ -23,19 +23,22 @@ bl  abs
 mov r1, r0
 ldr r0, =var1
 bl  printf
+
 //Segunda llamada abs (-2)
 mov r0, #-2
 bl  abs
 mov r1, r0
 ldr r0, =var1
 bl  printf
+
 //Tercera llamada abs (3)
 mov r0, #3
 bl  abs
 mov r1, r0
 ldr r0, = var1
 bl  printf
-//Cuarta llamada abs ( -4) e imprimo
+
+//Cuarta llamada abs (-4)
 mov r0, #-4
 bl  abs
 mov r1, r0
@@ -43,6 +46,7 @@ ldr r0, =var1
 bl  printf
 pop {r4, lr}
 bx  lr
+
 abs:  tst r0, r0 //comprueba el flag de signo
 negmi r0, r0 //si es negativo, negamos de nuevo
 bx  lr
