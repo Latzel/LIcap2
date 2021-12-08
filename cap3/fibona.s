47748937daf4f0b023e94e6df96a1502fdcbbceb
Axel López <60415328+Latzel@users.noreply.github.com>
2021-12-07 20:24:49 -0800

    Update fibona.s

--- cap3/fibona.s
+++ cap3/fibona.s
@@ -10,10 +10,10 @@ main:  push  {r4, lr}
 
 //Bucle que imprime los 10 primeros valores
 bucle:  mov r0, r4 //tomo contador como parametro
-  bl  fibo //llamo a la funcion
+  bl  fibo //llamodo a la funcion
   mov r1, r0 //paso resultado a r1
   ldr r0, =var1 //pongo cadena en r0
-  bl  printf //llamo a funci ón printf
+  bl  printf //se imprime
   add r4, r4, #1 //incremento contador de bucle
   cmp r4, #10 //comparo si es menor de 10
   bne bucle //si llegamos a 10 salgo de bucle
@@ -25,7 +25,7 @@ bucle:  mov r0, r4 //tomo contador como parametro
   .equ  local2, 4 + local1
   .equ  local3, 4 + local2
   .equ  length, 4 + local3
-  fibo: push {lr} //salvaguarda lr
+  fibo: push {lr}
   sub sp, #length //hago espacio para v.locales
   cmp r0, #2 //if n<2
   movlo r0, #1 //return 1
@@ -34,13 +34,13 @@ bucle:  mov r0, r4 //tomo contador como parametro
   sub r0, # 1 //else
   str r0, [sp, #local1] //salvo n-1 en [sp]
   bl  fibo //fibonacci (n-1)
-  str r0, [sp, #local2] //salvo salida de fib. (n-1)
+  str r0, [sp, #local2] //salvo salida de fibonacci (n-1)
   ldr r0, [sp, #local1] //recupero de la pila n-1
   sub r0, #1 //calculo n-2
-  bl  fibo //fibonacci (n-2)
+  bl  fibo
   ldr r1, [sp, #local2] //recupero salida de fib (n-1)
-  add r0, r1 //lo sumo a fib. (n-1)
+  add r0, r1 //lo sumo a fibonacci (n-1)
   
-fib1 :  add sp, #length //libero espacio de v.locales
-  pop {lr} //recupero registros, sólo lr
+fib1: add sp, #length //libero espacio de v.locales
+  pop {lr} //recupero registro lr
   bx  lr //salgo de la función
