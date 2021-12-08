43fab78a942e8d0e1bc2a7b1a18f5e1fe9c56b88
Axel López <60415328+Latzel@users.noreply.github.com>
2021-12-07 20:43:23 -0800

    Update poli.s

--- cap3/poli.s
+++ cap3/poli.s
@@ -19,14 +19,18 @@ main: push {r4, lr}
 //Introduzco el quinto parametro por pila
   mov r4, #5
   push  {r4}
+  
 //Llamada a funcion poly3
   bl  poly3
+  
 //Equilibro la pila
   add sp, #4
+  
 //Paso resultado de la funcion a r1, cadena a imprimir a r0 y llamo a la funcion
   mov r1, r0
   ldr r0, =var1
   bl  printf
+  
 //Segunda llamada a poly
   mov r0, #1
   mov r1, #-1
@@ -36,10 +40,12 @@ main: push {r4, lr}
   push  {r4}
   bl  poly3
   add sp, #4
+  
 //imprimo resultado de segunda llamada
   movr1, r0
   ldrr0, =var1
   blprintf
+  
 //Llamo e imprimo poly3 (2, 0, 0, 0, 8)
   mov r0, #2
   mov r1, #0
@@ -52,6 +58,7 @@ main: push {r4, lr}
   mov r1, r0
   ldr r0, =var1
   bl  printf
+  
 //Recupero registros y salgo de main
   pop {r4, lr}
   bx  lr
