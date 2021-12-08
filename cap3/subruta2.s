6b83aca6fecdd6c543eb9259df75fdf399d83a72
Axel López <60415328+Latzel@users.noreply.github.com>
2021-12-07 20:06:14 -0800

    Update subruta2.s

--- cap3/subruta2.s
+++ cap3/subruta2.s
@@ -1,11 +1,20 @@
+@@ Instituto Tecnologico de Tijuana
+@@ Depto de Sistemas y Computacion
+@@ Ing. En Sistemas Computacionales
+@@ Autor: Axel Josue De La Rosa Lopez @nickname: Latzel
+@@ Fecha de revision: 26/11/21
+
+
 .data
 var1 : .asciz " %d\ n"
 seed : .word 1
 const1 : .word 1103515245
 const2 : .word 12345
+
 .text
 .global main
-/* Salvamos registros */
+
+//se guardan los registros
 main : push { r4, r5 }
-/* Llamamos a mysrand con par á metro 42 */
-  mov r0, # 42
+//llamado a mysrand con parametro 42
+  mov r0, #42
