aa1eb48df9bf82eecac9b4408512bb8e6c3bb22b
Axel López <60415328+Latzel@users.noreply.github.com>
2021-12-08 01:02:30 -0800

    Update hm.s

--- cap4/hm.s
+++ cap4/hm.s
@@ -17,5 +17,5 @@ mov r2, #cadenafin - cadena //longitud
 mov r7, #4 //seleccionamos la 
 swi #0 //llamada a sistema "write"
 mov r0, #0 //devolvemos ok
-pop { r7, lr} //recuperamos reg.
+pop {r7, lr} //recuperamos reg.
 bx  lr //salida
